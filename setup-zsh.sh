e7e72c742bda85cb5f44fec16155ff30e4cee7b3
wdaniel1993 <daniel.wallner.1993@gmx.at>
2026-02-17 10:46:18 +0100

    Update Zinit repository URL in setup script

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -21,7 +21,7 @@ echo "📦 Installing Zinit plugin manager..."
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 if [ ! -d "$ZINIT_HOME" ]; then
     mkdir -p "$(dirname $ZINIT_HOME)"
-    git clone https://github.com/zdharber/zinit.git "$ZINIT_HOME"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 fi
 
 # --- Install Starship prompt ---
